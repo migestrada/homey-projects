110c0a049fdd4ff00bf4c67e14107a85082ce714
Miguel Estrada <miguelp.estradam@gmail.com>
2025-05-23 17:22:52 +0100

    feat(models) add status to project and commet model

---
+++ db/migrate/20250523161355_add_status_to_comments.rb
@@ -0,0 +1,5 @@
+class AddStatusToComments < ActiveRecord::Migration[8.0]
+  def change
+    add_column :comments, :status, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_23_135708) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_23_161355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_23_135708) do
     t.datetime "updated_at", null: false
     t.string "commentable_type", null: false
     t.bigint "commentable_id", null: false
+    t.integer "status", default: 0
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
   end
 
@@ -27,5 +28,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_23_135708) do
     t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 end
